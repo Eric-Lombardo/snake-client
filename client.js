4ec62fe03722969bf17db446fe559c49768dceb9
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-12 19:51:02 +0000

    added UI that will handle the ctrl+c keys to exit out of program

--- client.js
+++ client.js
@@ -19,10 +19,24 @@ const connect = function() {
   })
 
   // for connection handler
+  // write initials to the server
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: EJL")
   })
+
+  // move down
+  // conn.on("connect", () => {
+  //   // experiments with automatcially moving down on connect
+  //   // let x = 20
+  //   // for (let i = 0; i < 20; i++) {
+  //   //   x += 20
+  //   //   setTimeout(() => {
+  //   //     conn.write("Move: down");
+  //   //   }, x)
+  //   // }
+  // })
+
 
   return conn;
 };
--- play.js
+++ play.js
@@ -4,6 +4,32 @@
 
 const { connect } = require("./client");
 
+
+// setup user interface so that we can handle user input via stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+// will be used as a call back in setupInput to check if the key pressed
+// is ctrl+c and if it is to exit out of the program
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }  
+}
+
+
+
+
+
 // executing connect function from client.js
 console.log("connecting ....");
 connect();
+setupInput();
